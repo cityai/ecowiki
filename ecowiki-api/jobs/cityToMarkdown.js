2ec0f4aac3f02c05e7ac3eba158bb13dcbf95393
Aleksandar Polic <aleksandar.polic@marbleit.rs>
2019-08-15 16:55:35 +0200

    Working on logic for markdown conversion

--- ecowiki-api/jobs/cityToMarkdown.js
+++ ecowiki-api/jobs/cityToMarkdown.js
@@ -1,34 +1,44 @@
+const fs = require("fs");
+const path = require("path");
+
 const City = require("../models/city");
 const Community = require("../models/community");
-const fs = require("fs");
+
 class MarkdownTransform {
 
     async toMarkdown(location){
         const city =await City.findOne({name:location});
-        console.log(city.overview);
         const community = await Community.findById(city.community);
-        fs.readFile("/home/aleksandar/Desktop/home.md",(error,data)=>{
+
+        const filePath = path.join(__dirname,"..","..","ecowiki","content",location.toLowerCase(),"home.md");
+        const templatePath = path.join(__dirname,"..","..","ecowiki","content","cityTemplate.md");
+
+        fs.readFile(templatePath,(error,data)=>{
             if(error) throw error;
+            
             data = data.toString().split("\n");
-            let index = data.indexOf("# Overview");
-            index++;
-            let n = index;
-            data.splice(index,0,city.overview);
-            index++;
-            data = this.addMultipleLines(data,city,"events",5,"Events",["name","location"]);
-            data = this.addMultipleLines(data,city,"startups",5,"Startups",["name","description","link","investment"]);
-            fs.writeFileSync("/home/aleksandar/Desktop/home.md","");
+            data.splice(0,0,"<!-- TITLE: " + location + " AI -->");
+
+            data = this.addMultipleLines(data,city,"startups",3,"<div class=startups>",["name","categories","investment","description","link"]);
+            data = this.addMultipleLines(data,city,"events",5,"<div class=events>",["name","date","location","organizer","description"])
+            data = this.addMultipleLines(data,city,"organizations",5,"<div class=organizations>",["name"]);
+            data = this.addMultipleLines(data,community,"groups",5,"<div class=groups>",["name","members","category","organizer","description"]);
+            data = this.addMultipleLines(data,community,"influencers",5,"<div class=influencers>",["name","link"]);
+            fs.writeFileSync(filePath,"");
             for(let i =0;i<data.length;i++)
-                fs.appendFileSync("/home/aleksandar/Desktop/home1.md",data[i]+"\n");
+                fs.appendFileSync(filePath,data[i]+"\n");
         })
     }
 
-    addMultipleLines(data,document,docObj,n,sectionName,attributesArray){
+    addMultipleLines(data,document,docObj,n,setctionText,attributesArray){
         // data = data.toString().split("\n");
-        let index = data.indexOf("# "+sectionName);
+        let index = data.indexOf(setctionText);
+        index++;
+        data.splice(index,0,"");
         index++;
         for(let i=0;i<n;i++)
         {
+            if(document[docObj].length < 1) continue;
             for(let j=0;j<attributesArray.length;j++)
             {
                 
@@ -42,7 +52,9 @@ class MarkdownTransform {
                         index++;
                         break;
                     case "description":
-                        data.splice(index,0,"**Description:** " + document[docObj][i][attributesArray[j]]);
+                        if(!document[docObj][i][attributesArray[j]])
+                            document[docObj][i][attributesArray[j]] = "No description provided";
+                        data.splice(index,0,"**Description:** " + document[docObj][i][attributesArray[j]].replace(/(\r\n|\n|\r)/gm," "));
                         index++;
                         break;
                     case "members":
@@ -53,11 +65,19 @@ class MarkdownTransform {
                         data.splice(index,0,"**Investment in USD** " + document[docObj][i][attributesArray[j]])
                         index++;
                         break;
+                    case "categories":
+                        data.splice(index,0,document[docObj][i][attributesArray[j]].toString().replace(/,/g,", "));
+                        index++;
+                        break;
                     default:
                         data.splice(index,0,document[docObj][i][attributesArray[j]]);
                         index++;
+                
                 }
+                    
             }
+            data.splice(index,0,"");
+            index++;
         }
         return data;
     }
