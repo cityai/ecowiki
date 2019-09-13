0068a3ff7cdbcd515c30d34b2c37fcd0886c5dcb
Aleksandar Polic <aleksandar.polic@marbleit.rs>
2019-09-13 12:14:09 +0200

    Changed configuration files ./ecowiki/docker-compose.yml for staging. Also changed ./ecowiki/routes/meetupAuth.js so the redirect uri is the same as one registered on api.meetup for staging!

--- ecowiki-api/routes/meetupAuth.js
+++ ecowiki-api/routes/meetupAuth.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 
 const router = express.Router();
 
-router.get("/oauth",(req,res)=>{res.redirect("https://secure.meetup.com/oauth2/authorize?client_id=q31qm95rop67fmmcul0v9ta5id&response_type=code&redirect_uri=http://localhost:3000/api/meetup/www.wikiai.com")});
+router.get("/oauth",(req,res)=>{res.redirect("https://secure.meetup.com/oauth2/authorize?client_id=q31qm95rop67fmmcul0v9ta5id&response_type=code&redirect_uri=http://52.47.77.114:3000/api/meetup/www.wikiai.com")});
 router.get("/www.wikiai.com",async (req,res)=>{
     const ans = await fetch("https://secure.meetup.com/oauth2/access",
     {
@@ -16,7 +16,7 @@ router.get("/www.wikiai.com",async (req,res)=>{
         body:new URLSearchParams({"client_id":"q31qm95rop67fmmcul0v9ta5id",
             "client_secret":"mhim83of8cmqaq27u37lvl62bc",
             "grant_type":"authorization_code",
-            "redirect_uri":"http://localhost:3000/api/meetup/www.wikiai.com",
+            "redirect_uri":"http://52.47.77.114:3000/api/meetup/www.wikiai.com",
             "code":req.query.code})
     }).then(async json=>json.json())
     //SAVE ans.access_token i ans.refresh_token
