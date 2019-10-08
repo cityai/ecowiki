f3dc14a815056340d9c5f5fae2d5e888b4ee6d5a
Aleksandar Polic <aleksandar.polic@marbleit.rs>
2019-10-08 13:34:02 +0200

    Changed groups layout!

--- ecowiki-api/jobs/cityToMarkdown.js
+++ ecowiki-api/jobs/cityToMarkdown.js
@@ -97,7 +97,7 @@ class MarkdownTransform {
                     if (city.organizations)
                         data = this.addMultipleLines(data, city, "organizations", 5, "<div class=organizations>", ["name", "category", "founder", "link", "descriptions"]);
                     if (community) {
-                        data = this.addMultipleLines(data, community, "groups", 5, "<div class=groups>", ["name", "members", "category", "organizer", "description"]);
+                        data = this.addMultipleLines(data, community, "groups", 5, "<div class=groups>", ["name", "members", "category", "organizer"]);
                         data = this.addMultipleLines(data, community, "influencers", 5, "<div class=influencers>", ["name", "followers"]);
 
                     }
