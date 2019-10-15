a76ebfab6ec7cb3a5e674ce8122427b759f9bcfe
Aleksandar Polic <aleksandar.polic@marbleit.rs>
2019-10-15 10:35:53 +0200

    CRONs are setup for more cities to handle!

--- ecowiki-api/setup/setupCron.js
+++ ecowiki-api/setup/setupCron.js
@@ -8,8 +8,8 @@ const checker = require('../jobs/checkNewCitiesJob');
 const cityController = require("../controllers/cityController");
 
 module.exports = function setup(){
-    cron.schedule('*/10  * * * *', cityJob)
-    cron.schedule("*/3  * * * *",()=>{markdownJob.toMarkdown()});
-    cron.schedule("*/10  * * * *",async()=>{homePageJob();})
-    cron.schedule("*/1  * * * *",async()=>{checker.check()})
+    cron.schedule('0 0 */3 * * *', cityJob)
+    cron.schedule("0 0 */2 * * *",()=>{markdownJob.toMarkdown()});
+    cron.schedule("0 0 */1 * * *",async()=>{homePageJob();})
+    cron.schedule("*/30  * * * *",async()=>{checker.check()})
 }
