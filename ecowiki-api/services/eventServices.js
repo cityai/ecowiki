21b34562e6d8ba5c1b1fd84fce80d7968f96e479
Aleksandar Polic <aleksandar.polic@marbleit.rs>
2019-10-21 14:33:21 +0200

    Fixed error handling in event service!

--- ecowiki-api/services/eventServices.js
+++ ecowiki-api/services/eventServices.js
@@ -6,72 +6,76 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 global.Headers = fetch.Headers;
 
-class EventServices{
-    
+class EventServices {
+
     /**
      * Pulls events from MEETUP, and save them in our database.
      * @param location of city, as city name.
      */
-    async createEvents(location){
-        const refresh = process.env.MEETUP_REFRESH_TOKEN; 
-        const ans = await fetch('https://secure.meetup.com/oauth2/access',{
-            method:'POST',
+    async createEvents(location) {
+        const refresh = process.env.MEETUP_REFRESH_TOKEN;
+        const ans = await fetch('https://secure.meetup.com/oauth2/access', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
-              },
-            body: new URLSearchParams({'client_id':'q31qm95rop67fmmcul0v9ta5id',
-            'client_secret':'mhim83of8cmqaq27u37lvl62bc',
-            'grant_type':'refresh_token',
-            'redirect_uri':'http://localhost:3000/api/meetup/www.wikiai.com',
-            'refresh_token' : refresh
+            },
+            body: new URLSearchParams({
+                'client_id': 'q31qm95rop67fmmcul0v9ta5id',
+                'client_secret': 'mhim83of8cmqaq27u37lvl62bc',
+                'grant_type': 'refresh_token',
+                'redirect_uri': 'http://localhost:3000/api/meetup/www.wikiai.com',
+                'refresh_token': refresh
             })
         })
-        .then(json=>json.json());
-        
+            .then(json => json.json());
+
         const access = ans.access_token;
 
-        const locationData = await fetch('https://api.meetup.com/find/locations?&sign=true&photo-host=public&query='+ location + '&key=212b746b232c1e453431465736a56b',
+        const locationData = await fetch('https://api.meetup.com/find/locations?&sign=true&photo-host=public&query=' + location + '&key=212b746b232c1e453431465736a56b',
             {
-            method:'GET',
-            headers:new Headers({'Authorization':'Bearer ' + access}),
-          })
-            .then(res=>res.json());
-        
-        const lon = locationData[0].lon;
-        const lat =locationData[0].lat
-        
-        const response = await fetch('https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon='+ lon +'&page=100&text=Artificial Intelligence &radius=20 &lat='+ lat +'&key=212b746b232c1e453431465736a56b',
+                method: 'GET',
+                headers: new Headers({ 'Authorization': 'Bearer ' + access }),
+            })
+            .then(res => res.json()).catch(e => console.log(e));
+        let lon;
+        let lat;
+        if (locationData !== undefined) {
+            lon = locationData[0].lon;
+            lat = locationData[0].lat
+        }
+        const response = await fetch('https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=' + lon + '&page=100&text=Artificial Intelligence &radius=20 &lat=' + lat + '&key=212b746b232c1e453431465736a56b',
             {
-            method:'GET',
-            headers:new Headers({'Authorization':'Bearer ' + access}),
-          }).then(res=>res.json());
+                method: 'GET',
+                headers: new Headers({ 'Authorization': 'Bearer ' + access }),
+            }).then(res => res.json()).catch(e => console.log(e));
+        if (response) {
+            for (let i = 0; i < response.events.length; i++) {
 
-        for(let i=0; i<response.events.length; i++){
-            
-            const find = await Event.findOne({name: response.events[i].name})
-            if(find) continue;
-            
-            let description = '';
-            if(response.events[i].description) {
-                description = htmlRemover(response.events[i].description)
+                const find = await Event.findOne({ name: response.events[i].name })
+                if (find) continue;
+
+                let description = '';
+                if (response.events[i].description) {
+                    description = htmlRemover(response.events[i].description)
+                };
+                let address = 'No address provided';
+                if (response.events[i].venue && response.events[i].venue.address_1) {
+                    address = response.events[i].venue.address_1;
+                };
+                const event = new Event({
+                    name: response.events[i].name,
+                    date: response.events[i].local_date,
+                    location: location,
+                    address: address,
+                    organizer: response.events[i].group.name,
+                    link: response.events[i].link,
+                    description: description,
+                    highlighted: false,
+                    tags: null
+                });
+                await event.save();
             };
-            let address = 'No address provided';
-            if(response.events[i].venue && response.events[i].venue.address_1){
-                address = response.events[i].venue.address_1;
-            };
-            const event = new Event({
-                name: response.events[i].name,
-                date: response.events[i].local_date,
-                location: location,
-                address:address,
-                organizer: response.events[i].group.name,
-                link: response.events[i].link,
-                description: description,
-                highlighted: false, 
-                tags: null
-            });
-            await event.save();
-        };
+        }
         return response;
     };
 
@@ -79,9 +83,9 @@ class EventServices{
      * Returns events for the given location.
      * @param location of city, as city name.
      */
-    async getEvents(location){
-        const event = await Event.find({location: location});
-        if(!event) throw new ExtError(404, 'There are no events for given city!');
+    async getEvents(location) {
+        const event = await Event.find({ location: location });
+        if (!event) throw new ExtError(404, 'There are no events for given city!');
         return event;
     };
 
@@ -89,9 +93,9 @@ class EventServices{
      * Deletes an event from database by the given id.
      * @param id of event that we want to delete.
      */
-    async deleteEvent(id){
-        const event = await Event.findByIdAndDelete({_id: id});
-        if(!event) throw new ExtError(404, 'Event with the given ID was not found!');
+    async deleteEvent(id) {
+        const event = await Event.findByIdAndDelete({ _id: id });
+        if (!event) throw new ExtError(404, 'Event with the given ID was not found!');
         return event;
     };
 };
