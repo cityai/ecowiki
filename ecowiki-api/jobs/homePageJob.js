897949e858ef4a8de9f8474f1ffa2efa79bf19df
Aleksandar Polic <aleksandar.polic@marbleit.rs>
2019-10-24 12:43:34 +0200

    Added location to community groups on main page

--- ecowiki-api/jobs/homePageJob.js
+++ ecowiki-api/jobs/homePageJob.js
@@ -52,7 +52,7 @@ module.exports =async function(){
 
         data = MarkdownConvertor.addMultipleLinesFromArray(data,startups,10,"<div class=startups>",["name","categories","investment", "location"]);
         data = MarkdownConvertor.addMultipleLinesFromArray(data,influencers,10,"<div class=influencers>", ["name", "followers", "location"]);
-        data = MarkdownConvertor.addMultipleLinesFromArray(data,groups,10,"<div class=groups>", ["name", "members", "category", "organizer"]);
+        data = MarkdownConvertor.addMultipleLinesFromArray(data,groups,10,"<div class=groups>", ["name", "members", "category", "organizer", "location"]);
         data = MarkdownConvertor.addMultipleLinesFromArray(data,events,10,"<div class=events>", ["name", "date", "location", "organizer"]);
         data = MarkdownConvertor.addMultipleLinesFromArray(data,organizations,10,"<div class=organizations>", ["name"]);
         data = MarkdownConvertor.addMultipleLinesFromArray(data,cities,cities.length,'## A-Z',["cityLink"]);
